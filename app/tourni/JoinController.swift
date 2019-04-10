8c1cd912e2b4424b7192e5e2ebae14f238c2f1c0
April <owo@outlook.com>
2019-04-10 19:21:11 -0400

    keyboard constrains

--- app/tourni/JoinController.swift
+++ app/tourni/JoinController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class JoinController: UIViewController {
     
+    @IBOutlet weak var gameCodeTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,13 +19,9 @@ class JoinController: UIViewController {
     }
     
     
-    /*
-     // MARK: - Navigation
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @IBAction func keyboardDisableFunc(_ sender: UITapGestureRecognizer) {
+        gameCodeTextField.resignFirstResponder()
+    }
+    
     
 }
