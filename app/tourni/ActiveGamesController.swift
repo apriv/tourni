bc64841b41650d45f073f07b7896b84b0aa56d40
April <owo@outlook.com>
2019-04-10 18:02:04 -0400

    Revert "Major Change"
    This reverts commit 513111bbbf5badd80f912248ae42181fa11f3a37.

--- app/tourni/ActiveGamesController.swift
+++ app/tourni/ActiveGamesController.swift
@@ -9,79 +9,12 @@
 import UIKit
 import Firebase
 
-class ActiveGamesController: UITableViewController {
-    
-    // initialize a list for stored game names
-    var hosted_tournament_name_list = [String]()
-    
-    var joined_tournament_name_list = [String]()
-    
-    // Initialize a list for descriptions
-    var hosted_tournament_description_list = [String]()
-    
-    var joined_tournament_description_list = [String]()
-    
+class ActiveGamesController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    
-    // Set the number of rows for the list
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch(section){
-        case 0:
-            return self.joined_tournament_name_list.count
-        case 1:
-            return self.hosted_tournament_name_list.count
-        default: return 0
-            
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch(section){
-            case 0:
-                return "Joined Events"
-            case 1:
-                return "Hosted Events"
-            default: return "Silly devs made a coding error"
-        
-        }
-        
-    }
-    
-    // Set the text for each cell
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        
-        // Set the title to the tournament name
-        cell.textLabel?.text = self.hosted_tournament_name_list[indexPath.row]
-        // Set the description to the tournament description
-        cell.detailTextLabel?.text = self.hosted_tournament_description_list[indexPath.row]
-        
-        return cell
-    }
-    
-    // function called when the user leaves the view
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        // Reset list values when user leaves so the list can be updated when
-        // they return.
-        self.hosted_tournament_name_list = [String]()
-        self.hosted_tournament_description_list = [String]()
-    }
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
         // create reference to "user defaults" -> (data stored on phone)
         let defaults = UserDefaults.standard
@@ -90,53 +23,38 @@ class ActiveGamesController: UITableViewController {
         let db = Firestore.firestore()
         
         // initialize game_code_list
-        var hosted_game_code_list = [String]()
+        var game_code_list = [String]()
         
-        // if the game code list is valid, read from it
-        if UserDefaults.standard.object(forKey: "hosted_game_code_list") == nil {
-            UserDefaults.standard.setValue([String](), forKey: "hosted_game_code_list")
+        // retrieve the stored list from the phone
+        if UserDefaults.standard.object(forKey: "game_code_list") == nil {
+            UserDefaults.standard.setValue([String](), forKey: "game_code_list")
         }else{
-            hosted_game_code_list = defaults.object(forKey: "hosted_game_code_list") as? [String] ?? [String]()
+            game_code_list = defaults.object(forKey: "game_code_list") as? [String] ?? [String]()
         }
         
+        // initialize a list for stored game names
+        var game_name_list = [String]()
         
         // get active tournament information
-        // For each game code in the list of saved codes we query the database for
-        // the information of that tournament
-        for game_code in hosted_game_code_list{
+        for game_code in game_code_list{
             
-            // create document that we will be querying for with one of the
-            // game codes
             let docRef = db.collection("tournaments").document(game_code)
-            
             
             docRef.getDocument { (document, error) in
                 if let document = document, document.exists {
                     let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                    
                     print("Document data: \(dataDescription)")
-                    
-                    // get the name and description fields from the queried document
                     let name = document.get("name") as! String
-                    let description = document.get("description") as! String
-                    
-                    // append those values to our local lists
-                    self.hosted_tournament_name_list.append(name)
-                    self.hosted_tournament_description_list.append(description)
-                    
-                    print(self.hosted_tournament_name_list)
-                    
-                    // update the table with our newly retrieved data
-                    self.tableView.reloadData()
-                    
+                    game_name_list.append(name)
+                    print(game_name_list)
                 } else {
                     print("Document does not exist")
                 }
             }
             
         }
+        
     }
-    
     
 
     /*
