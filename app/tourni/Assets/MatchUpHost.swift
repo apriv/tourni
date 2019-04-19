742e6d4de567a9acf4d5b06766511683a75f011f
Shun Zhang <38138050+McZure@users.noreply.github.com>
2019-04-19 17:46:16 -0400

    Add files via upload

--- app/tourni/Assets/MatchUpHost.swift
+++ app/tourni/Assets/MatchUpHost.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class MatchUpHost: UITableViewCell {
 
+    @IBOutlet weak var Team1Label: Group1!
+    @IBOutlet weak var Team1Button: UIButton!
+    @IBOutlet weak var Team2Label: Group2!
+    @IBOutlet weak var Team2Button: UIButton!
+    
+    
+    @IBAction func ChangeTeam1Status(_ sender: UIButton) {
+        //When click on the button, the title on Team2Button changes from "WIN" to "LOSE"
+        
+        
+    }
     /*
-    // Only override draw() if you perform custom drawing.
+     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
